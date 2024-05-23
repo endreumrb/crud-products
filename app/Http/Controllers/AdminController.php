8e5498877b885e58e6bc22d303862ab1f4f8b7a3
Endreu Benites <endreu.matteusrb@gmail.com@gmail.com>
2024-05-23 20:03:32 -0300

    Add User Routes and Views

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.products');
+    }
+
+    public function show()
+    {
+        return view('admin.product_edit');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/product', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/admin/products', [AdminController::class, 'index'])->name('admin.product.index');
+
+Route::get('/admin/products/edit', [AdminController::class, 'show'])->name('admin.product.show');
